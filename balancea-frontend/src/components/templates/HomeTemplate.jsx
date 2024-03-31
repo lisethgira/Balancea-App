917e33990e6822b2382325d4fde048ab4c6b9bc9
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2024-03-31 15:36:42 -0500

    (fix): corriegiendo footer

--- balancea-frontend/src/components/templates/HomeTemplate.jsx
+++ balancea-frontend/src/components/templates/HomeTemplate.jsx
@@ -1,22 +1,29 @@
-import Articles  from "../atomos/articles"
-import Hero  from "../atomos/hero"
-import Info  from "../atomos/information"
+import Articles from "../atomos/articles";
+import Hero from "../atomos/hero";
+import Info from "../atomos/information";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-	overflow-x: hidden;
+  overflow-x: hidden;
+  min-height: 100vh; // Asegura una altura mínima del 100% de la altura de la ventana
+  display: flex;
+  flex-direction: column;
+`;
+export const Content = styled.div`
+  flex: 1; // Hace que este div crezca para ocupar el espacio disponible
 `;
 
-
 export default function HomeTemplate() {
-  
-  return (	<div>
-    <HomeContainer>
-      <Hero />
-      <Info />
-      <Articles />
-    </HomeContainer>
-  </div>
+  return (
+    <div>
+      <HomeContainer>
+        <Content>
+          <Hero />
+          <Info />
+          <Articles />
+        </Content>
+      </HomeContainer>
+    </div>
   );
 }
--- balancea-frontend/src/routes/routes.jsx
+++ balancea-frontend/src/routes/routes.jsx
@@ -2,23 +2,29 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import GlobalStyle from '../styles/globalStyle';
 
+import styled from "styled-components";
 //===============================================================================================================================================
 //========================================== Rutas principales  =================================================================================
 //===============================================================================================================================================
 const Header = lazy(() => import("../components/atomos/header"));
-const Footer = lazy(() => import("../components/atomos/footer"));
 const Home = lazy(() => import("../pages/Home"));
 const Login = lazy(() => import("../pages/Login"));
 const HomeUser = lazy(() => import("../pages/HomeUser"));
+const Footer = lazy(() => import("../components/atomos/footer"));
 
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
 
 const LandingPage = () => (
-  <>
+  <Content>
     <GlobalStyle />
     <Header />
     <Home />
     <Footer />
-  </>
+  </Content>
 );
 
 export function MyRoutes() {
