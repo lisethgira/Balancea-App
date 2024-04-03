4cdde1cf7a8c8c3b9a6eb4c6b954a1064991dfdd
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2024-04-03 01:43:27 -0500

    (fix): se quita service worker de la landing

--- landinBalancea/src/App.jsx
+++ landinBalancea/src/App.jsx
@@ -11,22 +11,6 @@ import Footer from './components/atomos/footer';
 
 const App = () => {
 
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-      navigator.serviceWorker.register("/service-worker.js").then(
-        function (registration) {
-          console.log(
-            "ServiceWorker registration successful with scope: ",
-            registration.scope
-          );
-        },
-        function (err) {
-          console.log("ServiceWorker registration failed: ", err);
-        }
-      );
-    });
-  }
-
   return (
     <div>
     <GlobalStyle />
